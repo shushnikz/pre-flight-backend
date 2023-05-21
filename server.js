e7fa2a848b3a3252085ef865606e59f50c7e1fb7
shushnikz <sushmithadevadiga997@gmail.com>
2023-05-21 19:39:55 +0530

    added comment

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
     res.send("Add Two Numbers")
 })
 
+// api end point to add two numbers
 app.post('/add', (req, res) => {
     const { number1, number2 } = req.body;
     const sum = Number(number1) + Number(number2);
